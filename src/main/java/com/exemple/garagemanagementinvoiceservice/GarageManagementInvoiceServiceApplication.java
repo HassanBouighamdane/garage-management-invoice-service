d5608d06ea8006fc3c37bffa902a26975bccc566
HassanBouighamdane <hassanbouighamdane2002@gmail.com>
2024-12-14 22:41:40 +0100

    first vommit

---
+++ src/main/java/com/exemple/garagemanagementinvoiceservice/GarageManagementInvoiceServiceApplication.java
@@ -0,0 +1,13 @@
+package com.exemple.garagemanagementinvoiceservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GarageManagementInvoiceServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GarageManagementInvoiceServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/exemple/garagemanagementinvoiceservice/GarageManagementInvoiceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exemple.garagemanagementinvoiceservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GarageManagementInvoiceServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
